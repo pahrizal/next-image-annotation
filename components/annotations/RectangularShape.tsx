8162c3793739b94e4bc783787ca577de60244690
PAHRIZAL MA'RUP <36581242+pahrizal@users.noreply.github.com>
2022-03-22 22:30:25 +0800

    init rect shape

---
+++ components/annotations/RectangularShape.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Konva from 'konva';
+import {Rect} from 'react-konva';
+
+type Props = {
+    id: string,
+    points:number[]
+    fillColor?: string,
+    strokeColor?: string,
+    strokeWidth?: number,
+}
+const RectangularShape:React.FC<Props> = ({
+    id,
+    points,
+    fillColor='#83CC1844',
+    strokeColor='#83CC18',
+    strokeWidth=4,
+})=>{
+    const ref = React.useRef<Konva.Rect>(null);
+    return (
+        <>
+            <Rect
+                ref={ref}
+                fill={fillColor}
+                strokeWidth={strokeWidth}
+                x={points[0]}
+                y={points[1]}
+                width={points[2] - points[0]}
+                height={points[3] - points[1]}
+                stroke={strokeColor}
+            />
+        </>
+    )
+}
+
+export default RectangularShape;
